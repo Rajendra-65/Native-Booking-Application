1718a53f512f8746d81e58ae84e04bf3f58fc859
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-01 16:15:53 +0530

    register route checked

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors"
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
+app.use(cors())
+app.use(express.json())
 app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,36 +1,43 @@
 import express from "express";
-import User from "../models/User";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken"
 
 const router = express.Router()
 
 const generateToken = (userId) => {
-    
+    return jwt.sign({userId},process.env.JWT_SECRET,{expiresIn: "15d"})
 }
 
 router.post("/register",async (req,res)=>{
     try{
-        const {email,username,password} = req.body;
+
+        console.log(req.body)
+        
+        const {email,username,password} = await req.body;
+        
         if(!username || !email || !password){
             return res.status(200).json({
                 message : "All fields are required"
             })
         }
+        
         if(password.length < 6){
             return res.status(500).json({
                 message: "Password must be 6 charecters long"
             })
         }
+        
         if(username.length < 3){
             return res.status(400).json({
                 message : "username mus t be at least 3 charecters long"
             })
         }
 
-        const exist = awiatUser.findOne({$or:[{email},{username}]})
         const existingUser = await User.findOne({email:email})
+        
         if(existingUser){
             return res.status(400).json({
-                message:"Email taken"
+                message:"Email already taken"
             })
         }
 
@@ -45,8 +52,18 @@ router.post("/register",async (req,res)=>{
         await user.save();
 
         const token = generateToken(user._id)
+
+        res.status(201).json({
+            token,
+            user : {
+                _id : user._id,
+                username : user.username,
+                email : user.email,
+                profileImage : user.profileImage
+            }
+        })
     }catch(e){
-        console.log(e)
+        console.log("Error in Register Route",e)
     }
 })
 
