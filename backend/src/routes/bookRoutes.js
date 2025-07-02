a4b24aa0e9dfa133759313eb776a2d93b82b88da
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-02 13:21:11 +0530

    Successfully configured the cloudinary and backend

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,5 +1,40 @@
 import express from "express";
-
+import cloudinary from "../lib/cloudinary";
+import Book from "../models/Book";
 const router = express.Router();
+
+router.post("/",async(req,res)=>{
+    try{
+        const { title , caption , rating , image } = req.body;
+
+        if(!image || !title || !caption || !rating ){
+            return res.status(400).json({
+                message: "please provide all details"
+            })
+        }
+
+        // upload to cloudinary
+        const result = await cloudinary.uploader.upload(image);
+        const imageUrl = result.secure_url;
+        // Save to Db
+        const newBook = new Book({
+            title,
+            caption,
+            rating,
+            image: imageUrl,
+    
+        })
+
+        await newBook.save()
+
+        res.status(201).json(newBook)
+
+    }catch(e){
+        console.log("error in the post route",e)
+        res.status(500).json({
+            message:e
+        })
+    }
+})
 
 export default router;
