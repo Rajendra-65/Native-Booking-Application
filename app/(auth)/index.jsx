30372cb41b866baa985125bd4d6da0a7d07a1b1f
Rajendra-65 <Rajendrbehera9@gmail.com>
2025-07-04 13:04:03 +0530

    Added the signup and login UI implemented successfully

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -1,47 +1,121 @@
-import { View, Image, Text } from "react-native";
+import {
+  View,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import styles from "../../assets/styles/login.styles";
 import { useState } from "react";
 import COLORS from "../../constatnts/colors";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowpassword] = useState(false);
-  const [isLoading,setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleLogin = () => {
-
-  }
+  const handleLogin = () => {};
 
   return (
-    <View
-        style = {styles.container}
+    <KeyboardAvoidingView
+      style={{
+        flex: 1,
+      }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
-      <View style = {styles.topIllustration}>
-        <Image 
-            source = {require("../../assets/images/Online-article-cuate.png")}
-            style = {styles.illustrationImage}
-            resizeMode = "contain"
-        />      
-      </View>
-      <View style = {styles.card}>
-        <View style = {styles.formContainer}>
-            <View style = {styles.inputGroup}>
-                <Text style = {styles.label}>Email</Text>
-                <View style = {styles.inputContainer}>
-                    <Ionicons
-                        name = "fill-outline"
-                        size= {20}
-                        color = {COLORS.primary}
-                        style = {styles.inputIcon}
-                    />
-                </View>
+      <View style={styles.container}>
+        <View style={styles.topIllustration}>
+          <Image
+            source={require("../../assets/images/Online-article-cuate.png")}
+            style={styles.illustrationImage}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.card}>
+          <View style={styles.formContainer}>
+            {/* Email */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Email</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="mail-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your Email"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={email}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+              </View>
             </View>
-
+            {/* password */}
+            <View style={styles.inputGroup}>
+              <Text style = {styles.label}>Password</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your password"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity
+                  onPress={() => setShowpassword(!showPassword)}
+                  style={styles.eyeIcon}
+                >
+                  <Ionicons
+                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                    size={20}
+                    color={COLORS.primary}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleLogin}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <ActivityIndicator color="#fff" />
+              ) : (
+                <Text style={styles.buttonText}>Login</Text>
+              )}
+            </TouchableOpacity>
+            {/* Footer */}
+            <View style={styles.footer}>
+                <Text style={styles.footerText}>
+                  Don&apos;t have an account ?
+                </Text>
+                <Link href="/signup" asChild>
+                  <TouchableOpacity>
+                    <Text style={styles.link}>Sign Up</Text>
+                  </TouchableOpacity>
+                </Link>
+            </View>
+          </View>
         </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
--- app/(auth)/signup.jsx
+++ app/(auth)/signup.jsx
@@ -1,12 +1,142 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import styles from "../../assets/styles/login.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constatnts/colors";
+import { useState } from "react";
+import { useRouter,Link } from "expo-router";
 
-const signup = () => {
+const Signup = () => {
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowpassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSignUp = () => {};
+
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView
+      style={{
+        flex: 1,
+      }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.container}>
+        <View style={styles.card}>
+          {/* Header */}
+          <View style={styles.header}>
+            <Text style={styles.title}>BookWorm</Text>
+            <Text style={styles.subtitle}>Share your favorite reads</Text>
+          </View>
+          {/* form container */}
+          <View style={styles.formContainer}>
+            {/* Username Inupt */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Username</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="person-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Jogn Doe"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={username}
+                  onChangeText={setUserName}
+                  autoCapitalize="none"
+                />
+              </View>
+            </View>
+            {/* password */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Email</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="mail-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your Email"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={email}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+              </View>
+            </View>
+            {/* password */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Password</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your password"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity
+                  onPress={() => setShowpassword(!showPassword)}
+                  style={styles.eyeIcon}
+                >
+                  <Ionicons
+                    name={showPassword ? "eye-outline" : "eye-off-outline"}
+                    size={20}
+                    color={COLORS.primary}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={handleSignUp}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <ActivityIndicator color="#fff" />
+              ) : (
+                <Text style={styles.buttonText}>Login</Text>
+              )}
+            </TouchableOpacity>
+            {/* Footer */}
+            <View style={styles.footer}>
+              <Text style={styles.footerText}>Already have an account ?</Text>
 
-export default signup
+                <TouchableOpacity
+                  onPress = {()=>router.back()}
+                >
+                  <Text style={styles.link}>Login</Text>
+                </TouchableOpacity>
+
+            </View>
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Signup;
